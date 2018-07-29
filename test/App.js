7a8b53f371c36fc5a70eb05294dc6081025e42cf
Gitea <gitea@fake.local>
2018-07-29 21:14:41 +0900

    .

--- test/App.js
+++ test/App.js
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 @connect(state => state)
 export default class App extends React.Component
 {
+	action(type, payload) 
+	{
+		return this.props.dispatch({ type, payload });
+	}
 	render()
 	{
 		return (
 			<div className="app">
-				appp
+				あっぷ
 			</div>
 		);
 	}
